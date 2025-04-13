5bf6ba7b3be31b862d761b38cd0ff9f0c313f067
WillianRRP <willianrenato754@gmail.com>
2025-04-13 17:24:29 -0300

    Criando um projeto em nexts

---
+++ meuprojeto/src/app/contatos/page.tsx
@@ -0,0 +1,8 @@
+export default function contatos(){
+    return(
+      <div>
+        <h1>oi</h1>
+        <span>(52) 9923</span>
+      </div>
+    )
+  }
---
+++ meuprojeto/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home(){
+  console.log('teste');
+  return(
+    <main>
+      <h1>oi</h1>
+      <span>seja bem vindo</span>
+    </main>
+  )
+}
