e41f08b4d2b14141b54cdee2825e993a0406f260
WillianRRP <willianrenato754@gmail.com>
2025-04-18 21:25:15 -0300

    server component

--- meuprojeto/src/app/page.tsx
+++ meuprojeto/src/app/page.tsx
@@ -1,9 +1,43 @@
-export default function Home(){
-  console.log('teste');
+interface DataProps {
+  id: number;
+  name: string;
+  full_name: string;
+  owner: {
+    login: string;
+    id: number;
+    avatar_url: string;
+    url: string;
+  }
+}
+
+
+async function getData(){
+  // http://api.github.com/users/WillianRRP/repos
+  const res = await fetch("http://api.github.com/users/WillianRRP/repos")
+    
+return res.json()
+
+}
+
+
+export default async function Home(){
+  const data:DataProps[] = await getData()
+ 
+
   return(
     <main>
       <h1>oi</h1>
       <span>seja bem vindo</span>
+      <br/>
+
+      <h3>Meus Repositóriods</h3>
+      {data.map((item) => (
+        <div key={item.id}>
+          <strong>Repositório:</strong> <a>{item.name}</a>
+          <br/><br/>
+        </div>  
+
+      ))}
     </main>
   )
 }
